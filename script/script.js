9705b1caee56115350d852884c96f2d2cc6d470b
АЙТА <73532620+SOADMFBE@users.noreply.github.com>
2021-03-26 14:01:48 +0900

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,10 @@
+/* document.onclick = function(event) {
+   if (document.querySelector('.none')) {
+        document.querySelector('.none').classList.replace('none', 'active');
+        console.log(document.querySelector('.active'));
+    }
+    else 
+    document.querySelector('.active').classList.replace('active', 'none');
+}
+
+ */
